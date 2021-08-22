7477bf5a7cd7ea67d2af2a523a5d2710531839df
gram <mail@orsinium.dev>
2021-08-22 12:11:07 +0200

    elements skeleton

---
+++ svg/elements.py
@@ -0,0 +1,410 @@
+from dataclasses import dataclass
+
+
+class Element:
+    pass
+
+
+@dataclass
+class SVG(Element):
+    pass
+
+
+@dataclass
+class G(Element):
+    pass
+
+
+@dataclass
+class Defs(Element):
+    pass
+
+
+@dataclass
+class Desc(Element):
+    pass
+
+
+@dataclass
+class Title(Element):
+    pass
+
+
+@dataclass
+class Symbol(Element):
+    pass
+
+
+@dataclass
+class Use(Element):
+    pass
+
+
+@dataclass
+class Image(Element):
+    pass
+
+
+@dataclass
+class Switch(Element):
+    pass
+
+
+@dataclass
+class Style(Element):
+    pass
+
+
+@dataclass
+class Path(Element):
+    pass
+
+
+@dataclass
+class Rect(Element):
+    pass
+
+
+@dataclass
+class Circle(Element):
+    pass
+
+
+@dataclass
+class Ellipse(Element):
+    pass
+
+
+@dataclass
+class Line(Element):
+    pass
+
+
+@dataclass
+class Polyline(Element):
+    pass
+
+
+@dataclass
+class Polygon(Element):
+    pass
+
+
+@dataclass
+class Text(Element):
+    pass
+
+
+@dataclass
+class Tspan(Element):
+    pass
+
+
+@dataclass
+class Tref(Element):
+    pass
+
+
+@dataclass
+class TextPath(Element):
+    pass
+
+
+@dataclass
+class AltGlyph(Element):
+    pass
+
+
+@dataclass
+class AltGlyphDef(Element):
+    pass
+
+
+@dataclass
+class AltGlyphItem(Element):
+    pass
+
+
+@dataclass
+class GlyphRef(Element):
+    pass
+
+
+@dataclass
+class Marker(Element):
+    pass
+
+
+@dataclass
+class ColorProfile(Element):
+    pass
+
+
+@dataclass
+class LinearGradient(Element):
+    pass
+
+
+@dataclass
+class RadialGradient(Element):
+    pass
+
+
+@dataclass
+class Stop(Element):
+    pass
+
+
+@dataclass
+class Pattern(Element):
+    pass
+
+
+@dataclass
+class ClipPath(Element):
+    pass
+
+
+@dataclass
+class Mask(Element):
+    pass
+
+
+@dataclass
+class Filter(Element):
+    pass
+
+
+@dataclass
+class FeDistantLight(Element):
+    pass
+
+
+@dataclass
+class FePointLight(Element):
+    pass
+
+
+@dataclass
+class FeSpotLight(Element):
+    pass
+
+
+@dataclass
+class FeBlend(Element):
+    pass
+
+
+@dataclass
+class FeColorMatrix(Element):
+    pass
+
+
+@dataclass
+class FeComponentTransfer(Element):
+    pass
+
+
+@dataclass
+class FeFuncR(Element):
+    pass
+
+
+@dataclass
+class FeFuncG(Element):
+    pass
+
+
+@dataclass
+class FeFuncB(Element):
+    pass
+
+
+@dataclass
+class FeFuncA(Element):
+    pass
+
+
+@dataclass
+class FeComposite(Element):
+    pass
+
+
+@dataclass
+class FeConvolveMatrix(Element):
+    pass
+
+
+@dataclass
+class FeDiffuseLighting(Element):
+    pass
+
+
+@dataclass
+class FeDisplacementMap(Element):
+    pass
+
+
+@dataclass
+class FeFlood(Element):
+    pass
+
+
+@dataclass
+class FeGaussianBlur(Element):
+    pass
+
+
+@dataclass
+class FeImage(Element):
+    pass
+
+
+@dataclass
+class FeMerge(Element):
+    pass
+
+
+@dataclass
+class FeMergeNode(Element):
+    pass
+
+
+@dataclass
+class FeMorphology(Element):
+    pass
+
+
+@dataclass
+class FeOffset(Element):
+    pass
+
+
+@dataclass
+class FeSpecularLighting(Element):
+    pass
+
+
+@dataclass
+class FeTile(Element):
+    pass
+
+
+@dataclass
+class FeTurbulence(Element):
+    pass
+
+
+@dataclass
+class Cursor(Element):
+    pass
+
+
+@dataclass
+class A(Element):
+    pass
+
+
+@dataclass
+class View(Element):
+    pass
+
+
+@dataclass
+class Script(Element):
+    pass
+
+
+@dataclass
+class Animate(Element):
+    pass
+
+
+@dataclass
+class Set(Element):
+    pass
+
+
+@dataclass
+class AnimateMotion(Element):
+    pass
+
+
+@dataclass
+class MPath(Element):
+    pass
+
+
+@dataclass
+class AnimateColor(Element):
+    pass
+
+
+@dataclass
+class AnimateTransform(Element):
+    pass
+
+
+@dataclass
+class Font(Element):
+    pass
+
+
+@dataclass
+class Glyph(Element):
+    pass
+
+
+@dataclass
+class MissingGlyph(Element):
+    pass
+
+
+@dataclass
+class HKern(Element):
+    pass
+
+
+@dataclass
+class VKern(Element):
+    pass
+
+
+@dataclass
+class FontFace(Element):
+    pass
+
+
+@dataclass
+class FontFaceSrc(Element):
+    pass
+
+
+@dataclass
+class FontFaceURI(Element):
+    pass
+
+
+@dataclass
+class FontFaceFormat(Element):
+    pass
+
+
+@dataclass
+class FontFaceName(Element):
+    pass
+
+
+@dataclass
+class DefinitionSrc(Element):
+    pass
+
+
+@dataclass
+class Metadata(Element):
+    pass
+
+
+@dataclass
+class ForeignObject(Element):
+    pass
