6d7b1c2081f93f7e72ba79eafe8288bc1f4842c7
gram <mail@orsinium.dev>
2021-10-22 14:57:41 +0200

    simplify grid generation

--- examples/grid4.py
+++ examples/grid4.py
@@ -21,33 +21,28 @@ SHIFT = int(STROKE_LEN / 2)
 STEP = STROKE_LEN + GAP
 
 
-def make_cross(x: int, y: int) -> svg.Path:
-    return svg.Path(
-        stroke="#2c3e50",
-        stroke_width=1,
-        stroke_linecap="round",
-        d=[
-            # horizontal line
-            svg.M(x - SHIFT, y),
-            svg.L(x + SHIFT, y),
-            # vertical line
-            svg.M(x, y - SHIFT),
-            svg.L(x, y + SHIFT),
-        ],
-    )
-
-
 def draw() -> svg.SVG:
-    canvas = svg.SVG(
-        viewBox=svg.ViewBoxSpec(0, 0, WIDTH, HEIGHT),
-        xmlns="http://www.w3.org/2000/svg",
-        elements=[],
-    )
-    assert canvas.elements is not None
+    lines = []
     for x in range(STROKE_LEN, WIDTH, STEP):
         for y in range(STROKE_LEN, HEIGHT, STEP):
-            canvas.elements.append(make_cross(x=x, y=y))
-    return canvas
+            lines.extend([
+                # horizontal line
+                svg.M(x - SHIFT, y),
+                svg.L(x + SHIFT, y),
+                # vertical line
+                svg.M(x, y - SHIFT),
+                svg.L(x, y + SHIFT),
+            ])
+    return svg.SVG(
+        viewBox=svg.ViewBoxSpec(0, 0, WIDTH, HEIGHT),
+        xmlns="http://www.w3.org/2000/svg",
+        elements=[svg.Path(
+            stroke="#2c3e50",
+            stroke_width=1,
+            stroke_linecap="round",
+            d=lines,
+        )],
+    )
 
 
 if __name__ == '__main__':
